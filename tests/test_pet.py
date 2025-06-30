f969ea514e830c4ac9a12e601c08aff684c3791a
igorBys <stevejobs69@MacBook-Air-steve.local>
2025-06-30 19:30:19 +0300

    Добавил изменнения в ожидаемый статус код

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -11,7 +11,7 @@ class TestPet:
             response = requests.delete(url=f'{BASE_URL}/pet/9999')
             print(response)
         with allure.step("Проверка статуса ответа"):
-            assert response.status_code == 201, "Код ответа не совпал с ожидаемым"
+            assert response.status_code == 200, "Код ответа не совпал с ожидаемым"
 
         with allure.step("Проверка текстового содержимого ответа"):
             assert response.text == "Pet deleted", "Текст ошибки не совпал с ожидаемым"
